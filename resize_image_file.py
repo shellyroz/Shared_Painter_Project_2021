ea97843369f6b578e7550b99aed0751ecedc23b5
shellyroz <74869899+shellyroz@users.noreply.github.com>
2021-05-30 17:57:45 +0300

    Delete resize_image_file.py

--- resize_image_file.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-# My image is a 200x374 jpeg that is 102kb large
-foo = Image.open("sailor_moon.png")
-# I downsize the image with an ANTIALIAS filter (gives the highest quality)
-foo = foo.resize((885, 500), Image.ANTIALIAS)
-# foo.save("small_sailor_moon.png", quality=95)
-# The saved downsized image size is 24.8kb
-foo.save("small_sailor_moon.png", quality=95, optimize=True)
-# The saved downsized image size is 22.9kb
